4847d1c2ebd0e0672fd4fbb7ac17c0250251ddc5
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-06-08 22:31:22 -0300

    Initial Commit

---
+++ pokemon-app/pokemon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_appApp.swift
+//  pokemon-app
+//
+//  Created by Renan Toniolo Rocha on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
